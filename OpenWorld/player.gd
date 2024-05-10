d4108f9885ef3ec03e51756e844d63aa2bf98792
Angeldust <simon.li@gadz.org>
2024-05-11 00:07:28 +0200

    obstacle + bouncy

--- OpenWorld/player.gd
+++ OpenWorld/player.gd
@@ -2,11 +2,12 @@ extends CharacterBody2D
 
 var speed = 400
 var friction = 0.01
-var acceleration = 0.01
-var rotation_speed = 0.5
+var acceleration = 0.02
+var rotation_speed = 1.0
 var rotation_acc = 0.1
 
 func _physics_process(delta):
+	# Movement manager
 	var ahead_vector = Vector2(0.0,-1.0).rotated(rotation)
 	var direction = Vector2.ZERO
 	var rotation_diretion = 0.0
@@ -25,4 +26,22 @@ func _physics_process(delta):
 		velocity = velocity.lerp(Vector2.ZERO, friction)
 	if velocity != Vector2.ZERO : 
 		rotation += rotation_diretion*rotation_speed*delta
+	
+	var temp_velocity = velocity*0.75
 	move_and_slide()
+	
+	# Bouncy bounce
+	if get_slide_collision_count() > 0:
+		var collision = get_slide_collision(0)
+		if collision != null:
+			velocity = temp_velocity.bounce(collision.get_normal())
+
+
+func _on_friction_zone_body_entered(body):
+	if body==self : 
+		speed /= 4
+
+
+func _on_friction_zone_body_exited(body):
+	if body==self : 
+		speed *= 4
