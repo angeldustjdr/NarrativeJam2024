c67bee4cb6e78d4da7162d5243682ea9bf7b053c
MetalGanon <georis.billo@gmail.com>
2024-05-14 01:57:21 +0200

    working loop for hub -> start mission 1 -> ilot 1 -> hub -> start mission 2

--- Hub/hub.gd
+++ Hub/hub.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
+@onready var open_world_packed_scene = preload("res://OpenWorld/open_world.tscn")
+
 func _ready():
+	GameState.check_mission_status()
+	$CanvasLayer/UI/CenterContainer/MarginContainer/VBoxContainer/ElectricalZone/ColorRect.gui_input.connect(self._on_mission_pressed)
 	Radio.connect("talkTo",talkTo)
 
 func talkTo(_who):
@@ -27,4 +31,8 @@ func clear(exclude):
 	for elem in $CanvasLayer/Control.get_children() : 
 		if elem != exclude : elem.visible = false
 
-
+func _on_mission_pressed(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			GameState.start_current_mission()
+			$scene_transition.transition_to_packed_scene(open_world_packed_scene)
---
+++ visual_novel/ilot_1.gd
@@ -0,0 +1,12 @@
+extends IlotGeneric
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.ilot_name = "ilot_1"
+	super()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
