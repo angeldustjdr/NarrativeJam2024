95149cf3f2cf8f246f9f9cd36b74a5705334e4bf
MetalGanon <georis.billo@gmail.com>
2024-05-23 16:28:53 +0200

    resume button in pause menu

--- pause_menu.gd
+++ pause_menu.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	self.set_process_mode(Node.PROCESS_MODE_WHEN_PAUSED)
 	if FileAccess.file_exists("res://saves/slot_1.save"):
 		%load_button.visible = true
 	else:
@@ -11,6 +12,7 @@ func _ready():
 	%load_button.pressed.connect(self._on_load_button_pressed)
 	%title_button.pressed.connect(self._on_title_button_pressed)
 	%desktop_button.pressed.connect(self._on_desktop_button_pressed)
+	%resume_button.pressed.connect(self._on_resume_button_pressed)
 
 func _on_save_button_pressed():
 	GameState.save_game_and_create_save_dir("slot_1.save")
@@ -31,3 +33,12 @@ func _on_desktop_button_pressed():
 	self.visible = false
 	get_tree().paused = false
 	SceneTransitionLayer.quit_game()
+	
+func _on_resume_button_pressed():
+	if GameState.is_pausable():
+		if get_tree().paused:
+			get_tree().paused = false
+			PauseMenu.visible = false
+		else:
+			get_tree().paused = true
+			PauseMenu.visible = true
